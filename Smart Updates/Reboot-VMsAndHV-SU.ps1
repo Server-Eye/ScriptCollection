b1a1892f0daca66326baed08539f01241d66cd1b
Leon Zewe <leon.zewe@server-eye.de>
2024-04-19 12:07:21 +0200

    General refactoring
    - Added $csvPath variable for reusibility purposes
    - Changed some variable names so that they better represent their purpose
    - Changed confusing For loops to While loops for better readability
    - Merged Replace-Quotes and Replace-Empty-Rows functions into one function

--- Smart Updates/Reboot-VMsAndHV-SU.ps1
+++ Smart Updates/Reboot-VMsAndHV-SU.ps1
@@ -9,7 +9,7 @@
     See this article for further information:
     https://servereye.freshdesk.com/support/solutions/articles/14000138445-anleitung-automatische-hyper-v-reboots-für-smart-updates-mit-script
 
-    .PARAMETER Time
+    .PARAMETER timeInMinutes
     Time in minutes that a VM can take to shut down before it is forcefully stopped.
     The default time is 30 minutes.
 	
@@ -25,47 +25,47 @@
 #>
 
 Param (
-    [Parameter(Mandatory=$false)][int]$Time = 30
+    [Parameter(Mandatory=$false)][int]$timeInMinutes = 30
 )
 
+# Path to the csv file
+$csvPath = "C:\vms.csv"
+
 function Reboot-HV {
-    $Servers = Import-Csv "C:\vms.csv"
+    $Servers = Import-Csv $csvPath
     # Sort by priority to shutdown
     $Servers = $Servers | Sort-Object { [int]$_.Prio } -Descending
     
     # Shut down VMs array
-    $Down = @()
+    $ShutdownList = @()
 
     # Setting up switch case ($False=shutdown,$True=start)
     $B = Test-Path 'HKLM:\SOFTWARE\ShutdownScript'
-    
-    # Set max time to shutdown
-    $T = $Time * 4 + 1 
 
     switch ($B) {
         $False {
             foreach ($Server in $Servers) {
-                # Shut down VM
                 Stop-VM -Name $Server.Name -Force
         
-                # Loop $T times (eq. max. $Time minutes)
-                for ($I = 1; $I -le $T; $I++){
-                    # Check if VM is down, if true add it to $Down, else wait 15sec and check again
-                    if ((Get-VM -Name $Server.Name).State -eq "Off"){
-                        $Down += $Server.Name
-                        Start-Sleep -Seconds 2
-                        $I = $T + 1
-                    } else {
-                        Start-Sleep -Seconds 15 
-                    }
+                $i = 0
+                # Loop until VM is off or max shutdown time is reached
+                while ((Get-VM -Name $Server.Name).State -ne "Off" -and $i -lt $timeInMinutes * 4) {
+                    Start-Sleep -Seconds 15
+                    $i++
                 }
-                # Forcefully stop the VM if its is still running $Time minutes after shutdown initiation
-                if (!($Down -contains $Server.Name)){
+
+                # If VM is off, add it to shutdown list
+                if ((Get-VM -Name $Server.Name).State -eq "Off") {
+                    $ShutdownList += $Server.Name
+                }
+
+                # Forcefully stop the VM if it is still running $timeInMinutes minutes after starting shutdown
+                if (-Not ($ShutdownList -Contains $Server.Name)) {
                     Stop-VM -Name $Server.Name -TurnOff
                 }
             }
             # Add and register scheduled task
-            $Para = "-ExecutionPolicy unrestricted -NonInteractive -WindowStyle Hidden -NoLogo -NoProfile -NoExit -File " + '"' + $CurrentScriptPath + '"'
+            $Para = "-ExecutionPolicy unrestricted -NonInteractive -WindowStyle Hidden -NoLogo -NoProfile -NoExit -File '$CurrentScriptPath'"
             $Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $Para
             $Option = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -WakeToRun
             $Trigger = New-JobTrigger -AtStartUp -RandomDelay (New-TimeSpan -Minutes 5)
@@ -82,10 +82,10 @@ function Reboot-HV {
             $Reason = "P"
             $Major = 0
             $Minor = 0
-            $Time = 1
+            $timeInMinutes = 1
             $PatchRun = "C:\Program Files (x86)\Server-Eye\triggerPatchRun.cmd"
             $FileToRunPath = "C:\WINDOWS\system32\shutdown.exe"
-            $Argument = '/r /t {0} /c "{1}" /d {2}:{3}:{4}' -f $Time, $Comment, $Reason, $Major, $Minor
+            $Argument = '/r /t {0} /c "{1}" /d {2}:{3}:{4}' -f $timeInMinutes, $Comment, $Reason, $Major, $Minor
             $StartProcessParams = @{
                 FilePath     = $FileToRunPath
                 ArgumentList = $Argument       
@@ -99,24 +99,24 @@ function Reboot-HV {
             $Servers = $Servers | Sort-Object { [int]$_.Prio }
             
             # Running VMs array
-            $Up = @()
+            $RunningVMs = @()
 
             foreach ($Server in $Servers) {
-        
                 # Boot VM
                 Start-VM -Name $Server.Name
-        
-                # Loop 60 times (eq. max. 15min)
-                for ($I = 1; $I -le 61; $I++){
-                    # Check if VM is up, if true add it to $Up, else wait 15sec and check again
-                    if ((Get-VM -Name $Server.Name).State -eq "Running"){
-                        $Up += $Server.Name
-                        $I = 61
-                        # Wait 120sec after successful boot
-                        Start-Sleep -Seconds 120
-                    } else {
-                        Start-Sleep -Seconds 15 
-                    }
+
+                $i = 0
+                # Loop until VM is up or max boot time is reached
+                while ((Get-VM -Name $Server.Name).State -ne "Running" -and $i -lt 61) {
+                    Start-Sleep -Seconds 15
+                    $i++
+                }
+
+                # If VM is up, add it to $RunningVMs
+                if ((Get-VM -Name $Server.Name).State -eq "Running") {
+                    $RunningVMs += $Server.Name
+                    # Wait 120sec after successful boot
+                    Start-Sleep -Seconds 120
                 }
             }
             # Remove regkey
@@ -129,17 +129,17 @@ function Reboot-HV {
 
 function Check-For-New-VM {
     # Import CSV
-    $Servers = Import-Csv "C:\vms.csv"
+    $Servers = Import-Csv $csvPath
     $VmPrio = 10
     
     # Names of currently active VMs
     $Cav = (Get-VM | Where-Object { $_.State -eq 'Running' }).Name
 
     foreach ($Vm in $Servers) {
-        if(!($Cav.Contains($Vm.Name))){
+        if(!($Cav.Contains($Vm.Name))) {
             # Delete inactive VM from CSV
-            $Servers | Where-Object { $_.Name -NotLike $Vm.Name } | Export-Csv "C:\vms.csv" -NoTypeInformation
-            $Servers = Import-Csv "C:\vms.csv"
+            $Servers | Where-Object { $_.Name -NotLike $Vm.Name } | Export-Csv $csvPath -NoTypeInformation
+            $Servers = Import-Csv $csvPath
         }
     }
 
@@ -148,11 +148,11 @@ function Check-For-New-VM {
     foreach($N in $Servers){ $ServerNames += $N.Name }
     
     foreach ($Vm in $Cav) {
-        if(!($ServerNames.Contains($Vm))){
+        if(!($ServerNames.Contains($Vm))) {
             # Add new VM to list with highest prio
             $NewLineVM = "{0},{1}" -f $Vm, $VmPrio
-            $NewLineVM | Add-Content -Path "C:\vms.csv"
-            $Servers = Import-Csv "C:\vms.csv"
+            $NewLineVM | Add-Content -Path $csvPath
+            $Servers = Import-Csv $csvPath
         }
     }
 }
@@ -160,29 +160,31 @@ function Check-For-New-VM {
 # Function to create vms.csv if not existent (does only run once)(all prios are set to 10)
 function Create-CSV {
     $ActiveVms = Get-VM | Where-Object { $_.State -eq 'Running' }
-    New-Item C:\vms.csv -ItemType File
-    Set-Content C:\vms.csv 'Name,Prio'
+    New-Item $csvPath -ItemType File
+    Set-Content $csvPath 'Name,Prio'
     $Prio = 10
     foreach($ActiveVm in $ActiveVms) {
         $NewLineVM = "{0},{1}" -f $ActiveVm.Name, $Prio
-        $NewLineVM | Add-Content -Path "C:\vms.csv" 
+        $NewLineVM | Add-Content -Path $csvPath
     }
 }
 
-function Replace-Quotes {
-    $Result = Get-Content -Path "C:\vms.csv"
-    $Result | ForEach-Object { $_ -replace '"', ""} | Set-Content "C:\vms.csv"
-}
-
-function Replace-Empty-Rows {
-    $Result = Get-Content -Path "C:\vms.csv"
-    $Result | ForEach-Object Trim | Where-Object Length -gt 0 | Set-Content "C:\vms.csv"
+function Replace-QuotesAndRows {
+    $Result = Get-Content -Path $csvPath
+    $Result = foreach ($line in $Result) {
+        $line = $line.Replace('"', "")
+        if ($line -ne "") {
+            $line = $line.Trim()
+        }
+        $line
+    }
+    $Result | Set-Content $csvPath
 }
 
 $CurrentScriptPath = $MyInvocation.MyCommand.Path
 
 # START
-if(!(Test-Path C:\vms.csv)) {
+if(!(Test-Path $csvPath)) {
     Create-CSV
     exit
 }
@@ -190,6 +192,5 @@ if(!(Test-Path HKLM:\SOFTWARE\ShutdownScript)) {
     Check-For-New-VM
 }
 
-Replace-Quotes
-Replace-Empty-Rows
+Replace-QuotesAndRows
 Reboot-HV
