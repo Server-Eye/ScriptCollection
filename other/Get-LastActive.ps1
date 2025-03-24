cacd5204db6ccf1da74f2b49465dde633703808c
servereye Helpdesk <sehelpdesk@servereye.de>
2025-03-24 14:04:05 +0000

    Resolve "Bug: Some containers are added twice to the list in getlastactive.ps1"

--- other/Get-LastActive.ps1
+++ other/Get-LastActive.ps1
@@ -2,33 +2,46 @@
 #Requires -Modules importexcel
 <# 
     .SYNOPSIS
-    Shows all System with no connection.
+    Shows all Systems that were not connected for a certain time or systems that have no current connection.
 
     .DESCRIPTION
-    Shows all System with no connection and the time of the last activity or Systems that were not connected for 14 Days or more.
+    Shows all Systems that were not connected for a certain time or systems that have no current connection.
+    The shown time is calculated based on the system time of the system the script is executed on.
 
-    Shows time based on the TimeZone of the System on which the Script was executed.
+    .PARAMETER LastActiveDays
+    How long should a system not have been connected to be displayed. Default is 14 days.
 
-    .PARAMETER LastActiveDays 
-    Last Active Days, default is 14
+    .PARAMETER PathToExcelFile 
+    If this parameter is passed, the result is exported to an Excel file at the specified location with the specified filename.
 
-    .PARAMETER PathtoExcelFile 
-    Excel File if one should be created
+    .PARAMETER NoCurrentConnection
+    If this parameter is passed, systems with no current connection are displayed.
 
-    .PARAMETER Apikey 
-    The api-Key of the user. ATTENTION only nessesary im no Server-Eye Session exists in den Powershell
-    
+    .PARAMETER ApiKey 
+    An Api-Key with Access to all customers. Only neccessary if not already authenticated via Connect-SESession.
+
+    .EXAMPLE
+    PS> .\Get-LastActive.ps1 -LastActiveDays 30 -PathToExcelFile "C:\temp\LastActiveSystems.xlsx"
+    Exports all systems that have not been connected for 30 days or more to an Excel file at the specified location.
+
+    PS> .\Get-LastActive.ps1 -NoCurrentConnection
+    Shows all systems that have no current connection in console.
 #>
 
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory = $false)]
     $LastActiveDays = "14",
+
     [Parameter(Mandatory = $false)]
-    $PathtoExcelFile,
+    [string]$PathToExcelFile,
+
+    [Parameter(Mandatory = $false)]
+    [switch]$NoCurrentConnection,
+
     [Parameter(ValueFromPipeline = $true)]
     [alias("ApiKey", "Session")]
-    $AuthToken
+    [string]$AuthToken
 )
 
 #region Server-Eye Default Variables
@@ -48,56 +61,66 @@ foreach ($customer in $customers) {
 
     $containers = Get-SeApiCustomerContainerList -AuthToken $AuthToken -CId $customer.id
 
-    foreach ($container in $containers) {
-        $tsp = New-TimeSpan -start $container.lastDate -End (Get-Date)
-        If ($container.subtype -eq "0" -and $container.message -ne $messageen -and $container.message -ne $messageen2 -and $container.message -ne $messagede) {
-            $result += [PSCustomObject]@{
-                Customer      = $customer.name
-                Network       = $container.name
-                System        = "OCC-Connector"
-                ID            = $container.ID
-                "Last Active" = $container.lastDate
-                Message       = $container.message
+    if ($NoCurrentConnection) {
+        foreach ($container in $containers) {
+            if ($container.subtype -eq "0" -and $container.message -ne $messageen -and $container.message -ne $messageen2 -and $container.message -ne $messagede) {
+                $result += [PSCustomObject]@{
+                    Customer      = $customer.name
+                    Network       = $container.name
+                    System        = "OCC-Connector"
+                    ID            = $container.ID
+                    "Last Active" = if ($container.lastDate) { $container.lastDate } else { "N/A" }
+                    Message       = $container.message
+                }
+            }
+            if ($container.subtype -eq "2" -and $container.message -ne $messageen -and $container.message -ne $messageen2 -and $container.message -ne $messagede) {
+                $occ = $containers | Where-Object { $_.id -eq $container.parentId }
+                $result += [PSCustomObject]@{
+                    Customer      = $customer.name
+                    Network       = $occ.name
+                    System        = $container.name
+                    ID            = $container.ID
+                    "Last Active" = if ($container.lastDate) { $container.lastDate } else { "N/A" }
+                    Message       = $container.message
+                } 
             }
         }
-        if ($container.subtype -eq "0" -and $tsp.TotalDays -gt $LastActiveDays) {
-            $result += [PSCustomObject]@{
-                Customer      = $customer.name
-                Network       = $container.name
-                System        = "OCC-Connector"
-                ID            = $container.ID
-                "Last Active" = $container.lastDate
-                Message       = $container.message
+    } else {
+        foreach ($container in $containers) {
+            if ($null -eq $container.lastDate) {
+                $tsp = New-TimeSpan -Start (Get-Date) -End (Get-Date)
+            } else {
+                $tsp = New-TimeSpan -Start $container.lastDate -End (Get-Date)
             }
-        }     
-        If ($container.subtype -eq "2" -and $container.message -ne $messageen -and $container.message -ne $messageen2 -and $container.message -ne $messagede) {
-            $occ = $containers | Where-Object { $_.id -eq $container.parentId }
-            $result += [PSCustomObject]@{
-                Customer      = $customer.name
-                Network       = $occ.name
-                System        = $container.name
-                ID            = $container.ID
-                "Last Active" = $container.lastDate
-                Message       = $container.message
-            } 
-        }      
-        If ($container.subtype -eq "2" -and $tsp.TotalDays -gt $LastActiveDays) {
-            $occ = $containers | Where-Object { $_.id -eq $container.parentId }
-            $result += [PSCustomObject]@{
-                Customer      = $customer.name
-                Network       = $occ.name
-                System        = $container.name
-                ID            = $container.ID
-                "Last Active" = $container.lastDate
-                Message       = $container.message
-            } 
-        }                            
+            
+            if ($container.subtype -eq "0" -and $tsp.TotalDays -gt $LastActiveDays) {
+                $result += [PSCustomObject]@{
+                    Customer      = $customer.name
+                    Network       = $container.name
+                    System        = "OCC-Connector"
+                    ID            = $container.ID
+                    "Last Active" = if ($container.lastDate) { $container.lastDate } else { "N/A" }
+                    Message       = $container.message
+                }
+            }
+            if ($container.subtype -eq "2" -and $tsp.TotalDays -gt $LastActiveDays) {
+                $occ = $containers | Where-Object { $_.id -eq $container.parentId }
+                $result += [PSCustomObject]@{
+                    Customer      = $customer.name
+                    Network       = $occ.name
+                    System        = $container.name
+                    ID            = $container.ID
+                    "Last Active" = if ($container.lastDate) { $container.lastDate } else { "N/A" }
+                    Message       = $container.message
+                }
+            }
+        }
     }
 }
+
 if ($PathtoExcelFile) {
     Export-Excel -Path $PathtoExcelFile -InputObject $result -AutoSize -AutoFilter
-}
-else {
+} else {
     Write-Output $result
 }
 
